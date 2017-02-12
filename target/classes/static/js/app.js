11003cfe34f72388cb346e7d201c8736dac549bf
Yuancheng Li <liyuancheng0118@gmail.com>
2017-02-12 04:30:13 -0800

    switch back to spring, commit html templates with angular material

---
+++ target/classes/static/js/app.js
@@ -0,0 +1,9 @@
+var app = angular.module('Timely', ['ngMaterial'])
+
+app.controller('contentCtrl', function ($scope, $mdSidenav) {
+  $scope.toShow = "appointment"; // Default
+
+  $scope.show = function (toShow) {
+    $scope.toShow = toShow;
+  };
+});
