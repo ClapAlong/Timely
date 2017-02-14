d69b9507cb66105ddfd90f658e9fb8208b0e8755
Yuancheng Li <liyuancheng0118@gmail.com>
2017-02-13 17:18:22 -0800

    update with full calendar plugin

--- target/classes/static/js/app.js
+++ target/classes/static/js/app.js
@@ -1,9 +1,10 @@
 var app = angular.module('Timely', ['ngMaterial'])
 
 app.controller('contentCtrl', function ($scope, $mdSidenav) {
-  $scope.toShow = "appointment"; // Default
+    $scope.toShow = "appointment"; // Default
 
-  $scope.show = function (toShow) {
-    $scope.toShow = toShow;
-  };
+    $scope.show = function (toShow) {
+        $scope.toShow = toShow;
+        $('#calendar').fullCalendar('render');
+    };
 });
