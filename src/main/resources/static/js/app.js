4540f5bff93804a36fdd49d5d0fd28606c20acb2
Yuancheng Li <liyuancheng0118@gmail.com>
2017-03-08 14:37:12 -0800

    early demo version

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -1,6 +1,4 @@
 var app = angular.module('Timely', ['ngMaterial'])
-var Date1;
-var Date2;
 
 app.controller('contentCtrl', function ($scope, $mdDialog) {
     //controller method for switching pages
@@ -11,31 +9,45 @@ app.controller('contentCtrl', function ($scope, $mdDialog) {
     };
 
     //controller method for date picker
-    $scope.myDate1 = new Date();
-    Date1 = $scope.myDate1;
-    $scope.myDate2 = new Date();
-    Date2 = $scope.myDate2;
+    $scope.myDate = new Date();
+    $scope.startInput = new Date();
+    $scope.endInput = new Date();
+
     $scope.minDate = new Date(
-        $scope.myDate1.getFullYear(),
-        $scope.myDate1.getMonth() - 2,
-        $scope.myDate1.getDate());
+        $scope.myDate.getFullYear(),
+        $scope.myDate.getMonth() - 2,
+        $scope.myDate.getDate());
     $scope.maxDate = new Date(
-        $scope.myDate1.getFullYear(),
-        $scope.myDate1.getMonth() + 2,
-        $scope.myDate1.getDate());
+        $scope.myDate.getFullYear(),
+        $scope.myDate.getMonth() + 2,
+        $scope.myDate.getDate());
     $scope.onlyWeekendsPredicate = function(date) {
         var day = date.getDay();
         return day === 0 || day === 6;
     }
 
     //Dialog controller
-    $scope.showPrerenderedDialog = function(ev) {
+    $scope.showAppointmentDialog = function(ev) {
         $mdDialog.show({
-            contentElement: '#myDialog',
+            contentElement: '#myDialogAppointment',
             parent: angular.element(document.body),
             targetEvent: ev,
             clickOutsideToClose: true
         });
     };
+
+    //Dialog controller
+    $scope.showOfficeHourDialog = function(ev) {
+        $mdDialog.show({
+            contentElement: '#myDialogOfficeHour',
+            parent: angular.element(document.body),
+            targetEvent: ev,
+            clickOutsideToClose: true
+        });
+    };
+
+    //Summary
+    $scope.sumA = new String;
+    $scope.sumOh = new String;
 
 });
--- src/main/resources/static/js/calendarCtrl.js
+++ src/main/resources/static/js/calendarCtrl.js
@@ -1,3 +1,13 @@
+var timeSlot
+var hourFromHtml;
+var buttonId;
+var sum;
+var locationTimely;
+var description;
+var attendee;
+
+
+
 $(document).ready(function() {
     $("#calendar-tab").click (function(){
         loadCalendar();
@@ -50,33 +60,206 @@ function loadCalendar()
     });
 }
 
+
+function getHourText(obj) {
+    hourFromHtml = obj.id;
+    console.log("Hour you clicked is " + hourFromHtml);
+}
+
+function getTimeSlotText(obj) {
+    var temp = obj.id;
+    if (temp == "0 - 15") {
+        timeSlot = "0";
+    } else if (temp == "15 - 30") {
+        timeSlot = "15";
+    } else if (temp == "30 - 45") {
+        timeSlot = "30";
+    } else {
+        timeSlot = "45";
+    }
+    console.log("Time slot you clicked is " + timeSlot);
+}
+
+
+function getId(obj) {
+    buttonId = obj.id;
+    console.log("Button you clicked is " + buttonId);
+}
+
 /**
  * Print the summary and start datetime/date of the next ten events in
  * the authorized user's calendar. If no events are found an
  * appropriate message is printed.
  */
-function insertEvents() {
-    var start= Date1;
+
+function insertOfficeHourEvents() {
+
+    var appElement = document.querySelector('[ng-app = Timely]');
+    var scope = angular.element(appElement).scope();
+
+    //start and end time
+    var DateParse = moment(scope.myDate).format();
+    var year = DateParse.split("T")[0];
+    var start = year + "T" + hourFromHtml + ":" + timeSlot + ":00";
+    var end = year + "T" + hourFromHtml + ":" + (parseInt(timeSlot)+15).toString() + ":00";
+
+    //assign parameter
+    if (buttonId == "Sun") {
+        sum = scope.sumOh;
+        locationTimely = "Cal Poly Pomona, Building 8 - Room 42, 3801 West Temple Avenue, Pomona, CA 91768";
+        description = "Appointment with Professor Sun Yu"
+        attendee = "yusun@cpp.edu";
+    } else if (buttonId == "Daisy") {
+        sum = scope.sumOh;
+        locationTimely = "Cal Poly Pomona, Building 8 - Room 49, 3801 West Temple Avenue, Pomona, CA 91768";
+        description = "Appointment with Professor Daisy F. Sang"
+        attendee = "liyuancheng0118@yahoo.com";
+    } else if (buttonId == "Tingting") {
+        sum = scope.sumOh;
+        locationTimely = "Cal Poly Pomona, Building 8 - Room 09, 3801 West Temple Avenue, Pomona, CA 91768";
+        description = "Appointment with Professor Tingting Chen"
+        attendee = "liyuancheng0118@yahoo.com";
+    } else if (buttonId == "Gilbert") {
+        sum = scope.sumOh;
+        locationTimely = "Cal Poly Pomona, Building 8 - Room 14, 3801 West Temple Avenue, Pomona, CA 91768";
+        description = "Appointment with Professor Gilbert Young"
+        attendee = "liyuancheng0118@yahoo.com";
+    } else {
+        sum = scope.sumA;
+        locationTimely = "Cal Poly Pomona, International Center (Building 1 Room 104), 3801 West Temple Avenue, Pomona, CA 91768";
+        description = "Appointment with International center"
+        attendee = calendarId;
+    }
+
+    console.log(sum)
+    console.log(locationTimely);
+    console.log(description);
+    console.log(attendee);
     console.log(start);
-    var end= Date1;
     console.log(end);
-    //var sum;
+
+
     var event = {
-        'summary': 'test',
-        'start':{
-            'date':start
+        'summary': sum,
+        'location': locationTimely,
+        'description': description,
+        'start': {
+            'dateTime': start,
+            'timeZone': 'America/Los_Angeles'
         },
-        'end':{
-            'date':end
+        'end': {
+            'dateTime': end,
+            'timeZone': 'America/Los_Angeles'
         },
+        'attendees': [
+            {'email': attendee},
+        ],
+        'reminders': {
+            'useDefault': false,
+            'overrides': [
+                {'method': 'email', 'minutes': 24 * 60},
+                {'method': 'popup', 'minutes': 10}
+            ]
+        }
     };
 
     var request = gapi.client.calendar.events.insert({
-        'calendarId': 'primary',
+        'calendarId': calendarId,
         'resource': event
     });
+
+
     request.execute(function(event) {
-        //appendPre('Event created: \n' + event.htmlLink);
+        //appendPre('Event created: ' + event.htmlLink);
         console.log('inserted');
     });
+
+}
+
+/**
+ * Print the summary and start datetime/date of the next ten events in
+ * the authorized user's calendar. If no events are found an
+ * appropriate message is printed.
+ */
+
+function insertAppointmentEvents() {
+
+    var appElement = document.querySelector('[ng-app = Timely]');
+    var scope = angular.element(appElement).scope();
+
+    //start and end time
+    var start = moment(scope.startInput).format();
+    var end = moment(scope.endInput).format();
+
+    //assign parameter
+    if (buttonId == "Sun") {
+        sum = scope.sumOh;
+        locationTimely = "Cal Poly Pomona, Building 8 - Room 42, 3801 West Temple Avenue, Pomona, CA 91768";
+        description = "Appointment with Professor Sun Yu"
+        attendee = "yusun@cpp.edu";
+    } else if (buttonId == "Daisy") {
+        sum = scope.sumOh;
+        locationTimely = "Cal Poly Pomona, Building 8 - Room 49, 3801 West Temple Avenue, Pomona, CA 91768";
+        description = "Appointment with Professor Daisy F. Sang"
+        attendee = "liyuancheng0118@yahoo.com";
+    } else if (buttonId == "Tingting") {
+        sum = scope.sumOh;
+        locationTimely = "Cal Poly Pomona, Building 8 - Room 09, 3801 West Temple Avenue, Pomona, CA 91768";
+        description = "Appointment with Professor Tingting Chen"
+        attendee = "liyuancheng0118@yahoo.com";
+    } else if (buttonId == "Gilbert") {
+        sum = scope.sumOh;
+        locationTimely = "Cal Poly Pomona, Building 8 - Room 14, 3801 West Temple Avenue, Pomona, CA 91768";
+        description = "Appointment with Professor Gilbert Young"
+        attendee = "liyuancheng0118@yahoo.com";
+    } else {
+        sum = scope.sumA;
+        locationTimely = "Cal Poly Pomona, International Center (Building 1 Room 104), 3801 West Temple Avenue, Pomona, CA 91768";
+        description = "Appointment with International center"
+        attendee = calendarId;
+    }
+
+    console.log(sum)
+    console.log(locationTimely);
+    console.log(description);
+    console.log(attendee);
+    console.log(start);
+    console.log(end);
+
+
+    var event = {
+        'summary': sum,
+        'location': locationTimely,
+        'description': description,
+        'start': {
+            'dateTime': start,
+            'timeZone': 'America/Los_Angeles'
+        },
+        'end': {
+            'dateTime': end,
+            'timeZone': 'America/Los_Angeles'
+        },
+        'attendees': [
+            {'email': attendee},
+        ],
+        'reminders': {
+            'useDefault': false,
+            'overrides': [
+                {'method': 'email', 'minutes': 24 * 60},
+                {'method': 'popup', 'minutes': 10}
+            ]
+        }
+    };
+
+    var request = gapi.client.calendar.events.insert({
+        'calendarId': calendarId,
+        'resource': event
+    });
+
+
+    request.execute(function(event) {
+        //appendPre('Event created: ' + event.htmlLink);
+        console.log('inserted');
+    });
+
 }
